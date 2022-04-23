5a5a1eb80e045abc160165fa4e46dc7a8414aa33
Oğuzhan Belli <oguzhanbellii@gmail.com>
2022-04-23 11:44:32 +0300

    Eslint config adding and Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="textlayer">Hello world!</h1>
     </div>
   );
 }
